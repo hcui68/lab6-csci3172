ae8c2e377135b2f693f18c5741aa5466713f033a
hcui68 <hj858960@dal.ca>
2022-03-08 17:47:09 -0400

    initial project

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Content />
+      <Content2 />
+      <Content3 />
+      <Content4 />
+      <Content5 />
+    </div>
+  );
+}
+
+function Content(){
+  return(
+    <div>
+      <p>This is a test!</p>
+    </div>
+  );
+}
+
+function Content2(){
+  return(
+    <div>
+      <p>Where is the test?</p>
+    </div>
+  );
+}
+
+function Content3(){
+  return(
+    <div>
+      <p>Right here!</p>
+    </div>
+  );
+}
+
+function Content4(){
+  return(
+    <div>
+      <p>Is this a test?</p>
+    </div>
+  );
+}
+
+function Content5(){
+  return(
+    <div>
+      <p>Yes, this is!</p>
+    </div>
+  );
+}
+
+export default App;
